daa332fde416f992cbe6ff19e2a6ec0fd76a8290
Abdul-pix <kwadisu4@gmail.com>
2026-05-08 12:52:25 +0200

    Update finish.py

--- finish.py
+++ finish.py
@@ -1,6 +1,6 @@
 """
 finish.py
-Finishlijn-klasse: de winconditie van het spel.
+De finishlijn die de speler moet bereiken om het level te winnen.
 """
 
 import pygame
@@ -9,14 +9,15 @@ from game_object import GameObject
 
 class Finish(GameObject):
     """
-    De finishlijn die de speler moet bereiken om het spel te winnen.
-    Bestaat uit een paal en een gele vlag.
-    Erft botsingsdetectie over van GameObject.
+    De finishlijn aan het einde van het level.
+    Als de speler hiermee botst, wint hij het spel.
+    Bestaat uit een witte paal met een gele vlag bovenaan.
+    Erft de botsingsdetectie over van GameObject.
     """
 
     def __init__(self, x, y):
         """
-        Initialiseert de finishlijn op de gegeven positie.
+        Maak de finishlijn aan op de gegeven positie.
 
         Args:
             x (float): Horizontale positie van de paal.
@@ -26,22 +27,14 @@ class Finish(GameObject):
 
     def draw(self, screen, cam_x):
         """
-        Tekent de finishlijn: een witte paal met een gele vlag.
+        Teken de finishlijn: een witte paal met een gele vlag erboven.
 
         Args:
-            screen (pygame.Surface): Het schermoppervlak.
-            cam_x (float): De camerapositie voor scrolling.
+            screen (pygame.Surface): Het scherm waarop we tekenen.
+            cam_x (float): De camerapositie voor horizontaal scrollen.
         """
         # Witte paal
-        pygame.draw.rect(
-            screen,
-            (230, 230, 230),
-            (self.x - cam_x, self.y, 5, self.height)
-        )
+        pygame.draw.rect(screen, (230, 230, 230), (self.x - cam_x, self.y, 5, self.height))
 
         # Gele vlag bovenaan de paal
-        pygame.draw.rect(
-            screen,
-            (255, 220, 0),
-            (self.x - cam_x + 5, self.y, 35, 25)
-        )
+        pygame.draw.rect(screen, (255, 220, 0), (self.x - cam_x + 5, self.y, 35, 25))
