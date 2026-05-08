9f74aebaea4025845174589e00f628be7b465161
Abdul-pix <kwadisu4@gmail.com>
2026-05-08 11:53:03 +0200

    Update game_object.py

--- game_object.py
+++ game_object.py
@@ -1,5 +1,4 @@
 from abc import ABC
-import pygame
 
 class GameObject(ABC):
     def __init__(self, x, y, w, h):
@@ -8,8 +7,18 @@ class GameObject(ABC):
         self.width = w
         self.height = h
 
-    def get_rect(self):
-        return pygame.Rect(self.x, self.y, self.width, self.height)
-
     def collides_with(self, other):
-        return self.get_rect().colliderect(other.get_rect())
+        left1 = self.x
+        right1 = self.x + self.width
+        top1 = self.y
+        bottom1 = self.y + self.height
+
+        left2 = other.x
+        right2 = other.x + other.width
+        top2 = other.y
+        bottom2 = other.y + other.height
+
+        if left1 < right2 and right1 > left2 and top1 < bottom2 and bottom1 > top2:
+            return True
+        else:
+            return False
