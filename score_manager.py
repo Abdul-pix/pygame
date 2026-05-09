a47787a250efc3b248fc284499d28cba72f63f9f
Abdul-pix <kwadisu4@gmail.com>
2026-05-09 19:39:43 +0200

    Update score_manager.py

--- score_manager.py
+++ score_manager.py
@@ -1,14 +1,41 @@
+"""
+score_manager.py
+Houdt de score bij tijdens het spel.
+Hoe sneller de speler de finish bereikt, hoe meer punten hij krijgt.
+"""
+
 import time
 
+
 class ScoreManager:
+    """
+    Dit beheert de score van de speler.
+    De score begint op 1000 en daalt met 10 punten per seconde.
+    Snel finishen = hoge score.
+    """
+
     def __init__(self):
-        self.start = time.time()
+        """Sla het starttijdstip op zodat we de verstreken tijd kunnen berekenen."""
+        self.start_tijd = time.time()
 
     def get_time(self):
-        return time.time() - self.start
+        """
+        Dit geeft terug hoeveel seconden er al gespeeld is.
+
+        Returns:
+            float: Verstreken tijd in seconden.
+        """
+        return time.time() - self.start_tijd
 
     def get_score(self):
-        s = 1000 - int(self.get_time() * 10)
-        if s < 0:
-            s = 0
-        return s
+        """
+        Dit berekent de huidige score op basis van de verstreken tijd.
+        De score kan niet onder 0 gaan.
+
+        Returns:
+            int: Huidige score tussen 0 en 1000.
+        """
+        score = 1000 - int(self.get_time() * 10)
+        if score < 0:
+            score = 0
+        return score
